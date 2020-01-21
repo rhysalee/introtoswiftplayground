e30d140ffb008bfc0cb16e223f03081b199f2bac
Rhysa <rhysalee@hawaii.edu>
2020-01-21 10:31:23 -1000

    Class 3 Jan. 21, 2020
    Completed steps 1-9

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var myFirstVariable: String
+
+myFirstVariable = "i've created my first string"
+
+//declare and assign variable in single line of code
+var mySecondVariable = "and look i added two strings together"
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print (myThirdVariable)
+
+var beachOne: String
+
+beachOne = "let's go north shore"
+
+var beachTwo = " and go swimming"
+
+var beachThree = " at the beach"
+
+var beachFour = beachOne + beachTwo + beachThree
+
+print (beachFour)
