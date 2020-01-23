3f7c8de2b113f1481e51bf4d1e2f1b85e054c67d
Rhysa <rhysalee@hawaii.edu>
2020-01-23 10:28:47 -1000

    Jan. 23, 2019
    Assignment 2 up to Part 8

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -26,9 +26,18 @@ var beachFour = beachOne + beachTwo + beachThree
 print (beachFour)
 
 //Assingment #2 Introduction to True and Flase
-
 var appState = false
 
 print (appState)
 
+var numberOne = 5
 
+var numberTwo = 7
+
+var comparison = numberOne < numberTwo
+
+var hikeOne = "makapuu"
+
+var hikeTwo = "makapuu"
+
+var compare = hikeOne == hikeTwo
